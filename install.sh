27c26b86a4d94f3dc46702ca20482afd16205834
Francisco Ciordia <gatupancho@gmail.com>
2022-08-05 18:34:46 -0300

    add: ejecutador que genera los enlaces

---
+++ install.sh
@@ -0,0 +1,9 @@
+#Kitty
+ln -s ~/.dotfiles/kitty.conf ./.config/kitty
+#git
+ln -s ~/.dotfiles/.gitconfig ~/
+#ZSH
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.p10k.zsh  ~/
+## Install Nv-chad config
+ln -s ~/.dotfiles/custom  ~/.config/nvim/lua/
